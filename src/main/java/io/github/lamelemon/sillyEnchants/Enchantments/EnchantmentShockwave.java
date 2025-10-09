81dbb02aed7fde71785009c6a03672952b642752
lamelemon <191560603+lamelemon@users.noreply.github.com>
2025-10-09 10:17:59 +0300

    Shockwave enchantment finished (its perfect :O)

--- src/main/java/io/github/lamelemon/sillyEnchants/Enchantments/EnchantmentShockwave.java
+++ src/main/java/io/github/lamelemon/sillyEnchants/Enchantments/EnchantmentShockwave.java
@@ -2,6 +2,7 @@ package io.github.lamelemon.sillyEnchants.Enchantments;
 
 import io.github.lamelemon.sillyEnchants.SillyEnchants;
 import io.github.lamelemon.sillyEnchants.Utils.EnchantmentUtil;
+import org.bukkit.damage.DamageType;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
@@ -11,24 +12,21 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.inventory.ItemStack;
 
 import static java.lang.Math.pow;
-import static org.joml.Math.*;
 
 public class EnchantmentShockwave implements Listener {
     private final String name = "shockwave";
     private final Enchantment enchantment = EnchantmentUtil.getEnchant(SillyEnchants.getPluginName() + ":" + name);
-    private final float maxFallDistance = 250;
-    private final float minFallDistance = 1.5f;
 
     @EventHandler
     private void onEntityHit(EntityDamageByEntityEvent event) {
-        SillyEnchants.getInstance().getLogger().info("trying to shockwave!" + enchantment + event.getDamager());
-        if (event.getDamager().getType() == EntityType.PLAYER && !event.getEntity().isInWater()) {
+        if (event.getDamageSource().getDamageType().equals(DamageType.MACE_SMASH) && event.getDamager().getType().equals(EntityType.PLAYER) && !event.getEntity().isInWater()) {
+
             Player player = (Player) event.getDamager();
             ItemStack item = player.getInventory().getItemInMainHand();
-            if (item.containsEnchantment(enchantment) && player.getFallDistance() > minFallDistance) {
-                int level = item.getEnchantmentLevel(enchantment);
-                player.getWorld().createExplosion(event.getEntity().getLocation(), (float) pow(level * min(player.getFallDistance(), maxFallDistance), 1.0/3.0));
-                SillyEnchants.getInstance().getLogger().info("Shockwave");
+
+            // fall distance above 1.5 due to mace smash attack having the same requirement
+            if (item.containsEnchantment(enchantment) && player.getFallDistance() >= 1.5) {
+                player.getWorld().createExplosion(player, event.getEntity().getLocation(), (float) pow(item.getEnchantmentLevel(enchantment) * player.getFallDistance(), 1.0/3.0), false, false);
             }
         }
     }
