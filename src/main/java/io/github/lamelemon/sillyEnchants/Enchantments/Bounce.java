0e7ea50d2dd8e40deffe3ee23a7c421c65ed7a2b
lamelemon <191560603+lamelemon@users.noreply.github.com>
2025-10-13 19:56:17 +0300

    bounce works

--- src/main/java/io/github/lamelemon/sillyEnchants/Enchantments/Bounce.java
+++ src/main/java/io/github/lamelemon/sillyEnchants/Enchantments/Bounce.java
@@ -11,9 +11,11 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.util.Vector;
 
+import java.util.HashMap;
 import java.util.Objects;
 
 public class Bounce implements Listener, CustomEnchantment {
@@ -29,7 +31,8 @@ public class Bounce implements Listener, CustomEnchantment {
         ItemStack bow = event.getBow();
         ItemStack arrow = event.getConsumable();
         if (bow != null && bow.containsEnchantment(enchantment) && arrow != null && Tag.ITEMS_ARROWS.isTagged(arrow.getType())) {
-            event.getProjectile().getPersistentDataContainer().set(arrowKey,
+            PersistentDataContainer dataContainer = event.getProjectile().getPersistentDataContainer();
+            dataContainer.set(arrowKey,
                     PersistentDataType.INTEGER,
                     bow.getEnchantmentLevel(enchantment)
             );
@@ -41,18 +44,29 @@ public class Bounce implements Listener, CustomEnchantment {
         Projectile projectile = event.getEntity();
         int bounces = Objects.requireNonNullElse(projectile.getPersistentDataContainer().get(arrowKey, PersistentDataType.INTEGER), 0);
 
-        if (event.getHitBlockFace() == null || bounces <= 0) return;
+        if (event.getHitBlockFace() == null || bounces <= 0 || projectile.getVelocity().length() < 0.5) return;
 
         projectile.getPersistentDataContainer().set(arrowKey, PersistentDataType.INTEGER, bounces - 1);
-        Vector normal = event.getHitBlockFace().getDirection();
-        Vector vel = projectile.getVelocity().clone().subtract(projectile.getVelocity().clone().multiply(normal).multiply(2));
-        SillyEnchants.getInstance().getLogger().info("vel " + vel + " velocity is " + projectile.getVelocity() + " hit block face " + event.getHitBlockFace() + " hit block direction " + normal);
 
-        projectile.teleport(normal.multiply(projectile.getWidth() / 2).add(projectile.getLocation().toVector()).toLocation(projectile.getWorld()));
+        Vector normal = event.getHitBlockFace().getDirection();
+        Vector startVel = projectile.getVelocity().clone();
+
+        double dot = startVel.dot(normal);
+        Vector reflect = startVel.clone().subtract(normal.clone().multiply(2 * dot));
+
+        SillyEnchants.getInstance().getLogger().info("startvel " + startVel + " velocity is " + projectile.getVelocity() + " hit block face " + event.getHitBlockFace() + " hit block direction " + normal);
+
+        projectile.teleport(
+                reflect.clone().multiply(0.05)
+                        .add(projectile.getLocation().toVector())
+                        .toLocation(projectile.getWorld())
+        );
 
         Bukkit.getScheduler().runTaskLater(SillyEnchants.getInstance(), () -> {
-            projectile.setVelocity(vel.multiply(3));
+            projectile.setVelocity(reflect);
             SillyEnchants.getInstance().getLogger().info("velocity is " + projectile.getVelocity());
-        },1);
+        },
+                1
+        );
     }
 }
